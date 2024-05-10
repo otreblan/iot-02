4c573425d3015a025d2148ee1c3231de3efff0e2
otreblan <otreblain@gmail.com>
2024-05-09 19:02:08 -0500

    Checkpoint 1

--- sketch_may09a/sketch_may09a.ino
+++ sketch_may09a/sketch_may09a.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <hcsr04.h>
 
+const int red_led = 20;
+const int green_led = 22;
+const int yellow_led = 24;
+
+
+
+HCSR04 hcsr04(51, 53, 20, 4000);
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(yellow_led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int distance = hcsr04.distanceInMillimeters();
+  Serial.println(distance);
+  
+  digitalWrite(yellow_led, (distance < 30*10) ? HIGH : LOW);
 }
