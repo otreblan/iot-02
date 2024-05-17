bf19c58fb269dc7644f6ab1076c1d3cec047679c
otreblan <otreblain@gmail.com>
2024-05-17 18:50:12 -0500

    feat(arduino): Add PlantWaterer

---
+++ sketch_may17a/plant_waterer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <LiquidCrystal.h>
+
+class PlantWaterer{
+private:
+  enum class state_t {
+    normal,
+    watering,
+    idle,
+    over_hydrated
+  };
+
+  const int blue_led;
+  const int red_led;
+  const int buzzer;
+  const int pump;
+
+  static const float low_humidity_threshold = 0.2;
+  static const float high_humidity_threshold = 0.8;
+
+  LiquidCrystal& lcd;
+  float current_humidity;
+  state_t current_state = state_t::normal;
+
+  state_t check_humidity(float humidity);
+  state_t get_next_state();
+  void reset_pins();
+  void print_humidity();
+  void state_handler();
+  
+public:
+  PlantWaterer(int blue_led, int red_led, int buzzer, int pump, LiquidCrystal& lcd);
+  void setup();
+  void loop();
+};
---
+++ sketch_may17a/plant_waterer.ino
@@ -0,0 +1,87 @@
+#include "plant_waterer.h"
+
+PlantWaterer::PlantWaterer(int blue_led, int red_led, int buzzer, int pump, LiquidCrystal& lcd):
+  blue_led(blue_led),
+  red_led(red_led),
+  buzzer(buzzer),
+  pump(pump),
+  lcd(lcd)
+{};
+
+PlantWaterer::state_t PlantWaterer::check_humidity(float humidity){
+  if(humidity < low_humidity_threshold) {
+    return state_t::watering;
+  }
+  else if(humidity > high_humidity_threshold) {
+    return state_t::over_hydrated;
+  }
+  else {
+    return state_t::normal;
+  }
+}
+
+PlantWaterer::state_t PlantWaterer::get_next_state(){
+  switch(current_state) {
+    case state_t::idle:
+      delay(15*1000);
+    case state_t::over_hydrated:
+    case state_t::normal:
+      return check_humidity(current_humidity);
+      
+    case state_t::watering:
+      delay(10*1000);
+      return state_t::idle;
+  }
+}
+
+void PlantWaterer::reset_pins(){
+  digitalWrite(blue_led, LOW);
+  digitalWrite(red_led, LOW);
+  digitalWrite(pump, LOW);
+  noTone(buzzer);
+  lcd.clear();
+}
+
+void PlantWaterer::print_humidity(){
+  lcd.print((int)(current_humidity*100));
+  lcd.print("%");
+} 
+
+void PlantWaterer::state_handler(){
+  reset_pins();
+  
+  switch(current_state){
+    case state_t::watering:
+      digitalWrite(blue_led, HIGH);
+      digitalWrite(pump, HIGH);
+      lcd.print("Regando...");
+      break;
+
+    case state_t::over_hydrated:
+      digitalWrite(red_led, HIGH);
+      tone(buzzer, 1000);
+    case state_t::normal:
+      print_humidity();
+      break;
+
+    case state_t::idle:
+      lcd.print("idle");
+      break;
+  }
+}
+
+void PlantWaterer::setup() {
+  pinMode(blue_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(pump, OUTPUT);
+  
+  lcd.begin(16,2);
+}
+
+void PlantWaterer::loop() {
+  current_humidity = 0; // TODO: Get humidity
+  
+  state_handler();
+  current_state = get_next_state();
+}
